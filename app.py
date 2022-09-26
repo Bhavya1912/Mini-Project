8df07e7edc31fd2d20a88bf9178c47078177c32c
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2022-09-26 20:03:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import os
 import gradio as gr
 
-
 print(os.popen(f'cat /etc/debian_version').read())
 print(os.popen(f'cat /etc/issue').read())
 print(os.popen(f'apt search tesseract').read())
 
 choices = os.popen('tesseract --list-langs').read().split('\n')[1:-1]
 
+
 def inference(filepath, languages):
     print('languages', languages)
     languages_str = ' -l ' + '+'.join(languages) if languages else ''
     print('languages_str', languages_str)
     return os.popen(f'tesseract {filepath} -{languages_str}').read()
+
 
 title = "Tesseract OCR"
 description = "Gradio demo for Tesseract. Tesseract is an open source text recognition (OCR) Engine."
